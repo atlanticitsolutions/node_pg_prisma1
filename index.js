4e383191c1102d1a2638f6f7b863aaaecf6d7b72
Atlantic IT Solutions <info@atlanticitsolutions.com>
2024-11-04 21:32:07 -0500

    added /health get route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Node Server is running..')
 })
 
+app.get('/health', (req, res) => {
+    res.send('Node Server is healthy.')
+  })
+  
 app.listen(3000, () => {
   console.log('App is listening on port 3000')
 })
