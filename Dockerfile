f204385468fbe1ffde67da9913999a5f6f91a958
Atlantic IT Solutions <info@atlanticitsolutions.com>
2024-11-05 21:06:53 -0500

    port to 3000 again

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,6 @@ COPY . .
 # Create JS Build
 # RUN npm run build
 
-EXPOSE 8080
+EXPOSE 3000
 
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use(express.json())
 app.use('/products', require('./routes/productRoute'))
 app.use('/categories', require('./routes/categoryRoute'))
 
-const port = process.env.PORT || 8080
-app.listen(port, () => {
-  console.log('Hello world listening on port', port)
-})
+const port = process.env.PORT || 3000
+// app.listen(port, () => {
+//   console.log('Hello world listening on port', port)
+// })
 
-// app.listen(3000, () => console.log('Server started on port 3000'))
+app.listen(port, () => console.log('Server started on port ' + port))
