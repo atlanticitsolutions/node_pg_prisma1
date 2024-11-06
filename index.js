437b38e07aa2787b8bd59ebeec123278ba56e0d3
Atlantic IT Solutions <info@atlanticitsolutions.com>
2024-11-05 21:22:42 -0500

    port 8080 in index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use('/products', require('./routes/productRoute'))
 app.use('/categories', require('./routes/categoryRoute'))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 // app.listen(port, () => {
 //   console.log('Hello world listening on port', port)
 // })
